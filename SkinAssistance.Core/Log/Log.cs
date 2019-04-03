f4d6712edd58e243aee73d22fc479917a764566a
Tearies <maojun_huier@sina.com>
2019-04-03 17:55:17 +0800

    调整目录结构

---
+++ SkinAssistance.Core/Log/Log.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using NLog.Config;
+
+namespace SkinAssistance.Core
+{
+    public static class FileHelper
+    {
+         
+      
+
+     
+
+       
+        
+ 
+        #region 获取文件相对路径映射的物理路径
+        /// <summary>
+        /// 获取文件相对路径映射的物理路径，若文件为绝对路径则直接返回
+        /// </summary>
+        /// <param name="relativePath">文件的相对路径</param>        
+        public static string GetPhysicalPath(string relativePath)
+        {
+            //有效性验证
+            if (string.IsNullOrEmpty(relativePath))
+            {
+                return string.Empty;
+            }
+            //~,~/,/,\
+            relativePath = relativePath.Replace("/", @"\").Replace("~", string.Empty).Replace("~/", string.Empty);
+            //网络共享目录中的文件不应移除根路径
+            if (!relativePath.StartsWith("\\\\"))
+                relativePath = relativePath.StartsWith("\\") ? relativePath.Substring(1, relativePath.Length - 1) : relativePath;
+            var path = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            var fullPath = System.IO.Path.Combine(path, relativePath);
+            return fullPath;
+        }
+        #endregion
+
+        
+ 
+    }
+    public class NLogger : ILogger
+    {
+        private NLog.ILogger _DefaultLogger;
+
+        public NLogger()
+        {
+            this.LoadLogConfig();
+        }
+
+        private void LoadLogConfig()
+        {
+            NLog.Config.XmlLoggingConfiguration config = new XmlLoggingConfiguration(FileHelper.GetPhysicalPath("config\\nlog.config"), false);
+            var factory = new NLog.LogFactory(config);
+            _DefaultLogger = factory.GetLogger("DefaultLog");
+        }
+
+        /// <summary>
+        /// 更改日志存储路径
+        /// </summary>
+        /// <param name="name"></param>
+        public void UpdateLogPath(Func<string, string> func)
+        {
+            NLog.Config.XmlLoggingConfiguration config = new XmlLoggingConfiguration(FileHelper.GetPhysicalPath("config\\nlog.config"), true);
+            foreach (var item in config.AllTargets)
+            {
+                var ftarget = item as NLog.Targets.FileTarget;
+                if (ftarget == null) continue;
+                var lay = ftarget.FileName as NLog.Layouts.SimpleLayout;
+                lay.Text = func(lay.Text);
+            }
+            var factory = new NLog.LogFactory(config);
+            _DefaultLogger = factory.GetLogger("DefaultLog");
+        }
+
+        public void Info(string message)
+        {
+            _DefaultLogger.Info(message);
+        }
+
+        public void Warn(string message)
+        {
+            _DefaultLogger.Warn(message);
+        }
+
+        public void Error(string message, Exception ex = null)
+        {
+            _DefaultLogger.Error(message + Environment.NewLine + ex.ToString());
+        }
+
+        public void Debug(string message)
+        {
+            _DefaultLogger.Debug(message);
+        }
+
+        /// <summary>
+        /// 记录Trace信息
+        /// </summary>
+        /// <param name="message"></param>
+        public void Trace(string message)
+        {
+
+            _DefaultLogger.Trace(message);
+        }
+    }
+    public interface ILogger
+    {
+        /// <summary>
+        /// 记录消息日志
+        /// </summary>
+        void Info(string message);
+
+        /// <summary>
+        /// 向日志文件中写入 “警告内容”。
+        /// </summary>
+        void Warn(string message);
+
+        /// <summary>
+        /// 记录错误消息
+        /// </summary>
+        void Error(string message, Exception ex = null);
+
+        /// <summary>
+        /// 记录调试信息
+        /// </summary>
+        void Debug(string message);
+
+        /// <summary>
+        /// 记录Trace信息
+        /// </summary>
+        /// <param name="message"></param>
+        void Trace(string message);
+    }
+    public static class Log
+    {
+        private static ILogger InnerLoger;
+
+        static Log()
+        {
+            InnerLoger = new NLogger();
+        }
+
+        /// <summary>
+        /// 更改日志存储路径
+        /// </summary>
+        public static void UpdateLogPath(Func<string, string> func)
+        {
+            (InnerLoger as NLogger).UpdateLogPath(func);
+        }
+
+        /// <summary>
+        /// 记录消息日志
+        /// </summary>
+        public static void Info(string message)
+        {
+            InnerLoger.Info(message);
+        }
+        /// <summary>
+        /// 向日志文件中写入 “警告内容”。
+        /// </summary>
+        public static void Warn(string message)
+        {
+            InnerLoger.Warn(message);
+        }
+        /// <summary>
+        /// 记录错误消息
+        /// </summary>
+        public static void Error(string message, Exception ex = null)
+        {
+            InnerLoger.Error(message, ex);
+        }
+        /// <summary>
+        /// 记录调试信息
+        /// </summary>
+        public static void Debug(string message)
+        {
+            InnerLoger.Debug(message);
+        }
+
+        /// <summary>
+        /// 向日志文件中写入Trace
+        /// </summary>
+        public static void Trace(string message)
+        {
+            InnerLoger.Trace(message);
+        }
+    }
+}
---
+++ SkinAssistance.Core/Log/LogExtensions.cs
@@ -0,0 +1,120 @@
+﻿#region NS
+
+using System;
+using SkinAssistance.Core.Extensions;
+using SkinAssistance.Core.Native;
+
+#endregion
+
+namespace SkinAssistance.Core
+{
+    #region Reference
+
+    #endregion
+
+    /// <summary>
+    /// Class LogExtensions.
+    /// </summary>
+    public static class LogExtensions
+    {
+        #region Method
+        /// <summary>
+        /// Informations the specified string format.
+        /// </summary>
+        /// <param name="logSource">The log source.</param>
+        /// <param name="stringFormat">The string format.</param>
+        /// <param name="params">The parameters.</param>
+        public static void Info(this object logSource, string stringFormat, params object[] @params)
+        {
+            Log.Info(GetThreadInfo() + stringFormat.SafeFormat(@params));
+        }
+
+        /// <summary>
+        /// Debugs the specified string format.
+        /// </summary>
+        /// <param name="logSource">The log source.</param>
+        /// <param name="stringFormat">The string format.</param>
+        /// <param name="params">The parameters.</param>
+        public static void Debug(this object logSource, string stringFormat, params object[] @params)
+        {
+            Log.Debug(GetThreadInfo() + stringFormat.SafeFormat(@params));
+        }
+
+        /// <summary>
+        /// Errors the specified string format.
+        /// </summary>
+        /// <param name="logSource">The log source.</param>
+        /// <param name="stringFormat">The string format.</param>
+        /// <param name="params">The parameters.</param>
+        public static void Error(this object logSource, string stringFormat,
+            params object[] @params)
+        {
+            Log.Error(GetThreadInfo() + stringFormat.SafeFormat(@params));
+        }
+
+        /// <summary>
+        /// Errors the specified ex.
+        /// </summary>
+        /// <param name="logSource">The log source.</param>
+        /// <param name="ex">The ex.</param>
+        /// <param name="stringFormat">The string format.</param>
+        /// <param name="params">The parameters.</param>
+        public static void Error(this object logSource, Exception ex, string stringFormat, params object[] @params)
+        {
+            Log.Error(GetThreadInfo() + stringFormat.SafeFormat(@params), ex);
+        }
+
+        /// <summary>
+        /// Errors the specified ex.
+        /// </summary>
+        /// <param name="logSource">The log source.</param>
+        /// <param name="ex">The ex.</param>
+        public static void Error(this object logSource, Exception ex)
+        {
+            Log.Error(GetThreadInfo(), ex);
+        }
+
+        /// <summary>
+        /// Criticals the specified string format.
+        /// </summary>
+        /// <param name="logSource">The log source.</param>
+        /// <param name="stringFormat">The string format.</param>
+        /// <param name="params">The parameters.</param>
+        public static void Critical(this object logSource, string stringFormat, params object[] @params)
+        {
+            Log.Error(GetThreadInfo() + stringFormat.SafeFormat(@params));
+        }
+
+        /// <summary>
+        /// Criticals the specified exception.
+        /// </summary>
+        /// <param name="logSource">The log source.</param>
+        /// <param name="exception">The exception.</param>
+        public static void Critical(this object logSource, Exception exception)
+        {
+            Log.Error(GetThreadInfo() , exception);
+        }
+
+        /// <summary>
+        /// Warnings the specified string format.
+        /// </summary>
+        /// <param name="logSource">The log source.</param>
+        /// <param name="stringFormat">The string format.</param>
+        /// <param name="params">The parameters.</param>
+        public static void Warning(this object logSource, string stringFormat, params object[] @params)
+        {
+            Log.Warn(GetThreadInfo() + stringFormat.SafeFormat(@params));
+        }
+
+
+        /// <summary>
+        /// Gets the thread information.
+        /// </summary>
+        /// <returns>System.String.</returns>
+        private static string GetThreadInfo()
+        {
+            return "UnManaged ID:[" + ThreadHelper.GetCurrentThreadID() + "]| ";
+        }
+        #endregion
+    }
+}
